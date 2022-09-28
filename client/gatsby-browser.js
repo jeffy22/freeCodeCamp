cb7d10154bd69e7ed23f82d5aa455af78742a3f0
jeffy22 <jeff.page019@gmail.com>
2022-09-27 21:45:22 -0500

    commit message

---
+++ client/gatsby-browser.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+
+import { createStore } from './src/redux/createStore';
+import AppMountNotifier from './src/components/AppMountNotifier';
+import layoutSelector from './utils/gatsby/layoutSelector';
+
+require('typeface-lato');
+require('typeface-roboto-mono');
+
+const store = createStore();
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <Provider store={store}>
+      <AppMountNotifier render={() => element} />
+    </Provider>
+  );
+};
+
+wrapRootElement.propTypes = {
+  element: PropTypes.any
+};
+
+export const wrapPageElement = layoutSelector;
+
+export const disableCorePrefetching = () => true;
